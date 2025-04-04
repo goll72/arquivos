d2ae07e35216f52ad05248aaaa958e84c72c1ab7
goll <joaog.bastos@protonmail.ch>
2025-04-04 16:57:24 -0300

    Implementação inicial da funcionalidade 2 "SELECT *"

---
+++ include/error.h
@@ -0,0 +1,8 @@
+#ifndef ERROR_H
+#define ERROR_H
+
+/** Definição de mensagens de erro */
+#define E_NOREG "Registro inexistente."
+#define E_PROCESSINGFILE "Falha no processamento do arquivo."
+
+#endif /* ERROR_H */
--- src/main.c
+++ src/main.c
@@ -1,7 +1,112 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
+#include "defs.h"
+#include "hash.h"
 #include "file.h"
+#include "error.h"
+
+enum functionality {
+     FUNC_CREATE_TABLE = 1,
+     FUNC_SELECT_STAR = 2,
+     FUNC_SELECT_WHERE = 3
+};
+
+static void free_var_data_fields(f_data_reg_t *reg)
+{
+#define X(_, name)
+#define Y(_, name) free(reg->name);
+
+     DATA_REG_FIELDS(X, Y)
+
+#undef X
+#undef Y
+}
 
 int main(void)
 {
+     enum functionality func;
+     int ret = scanf("%d", &func);
+
+     if (ret != 1) {
+          puts(E_PROCESSINGFILE);
+          return 1;
+     }
+
+     switch (func) {
+          case FUNC_CREATE_TABLE:
+               {
+                    char csv_file[PATH_MAX];
+                    char bin_file[PATH_MAX];
+
+                    ret = scanf("%s %s", csv_file, bin_file);
+
+                    if (ret != 2) {
+                         puts(E_PROCESSINGFILE);
+                         return 1;
+                    }
+
+                    /* ... */
+               }
+               
+               break;
+          case FUNC_SELECT_STAR:
+               {
+                    char bin_file[PATH_MAX];
+
+                    ret = scanf("%s", bin_file);
+
+                    if (ret != 1) {
+                         puts(E_PROCESSINGFILE);
+                         return 1;
+                    }
+
+                    FILE *f = fopen(bin_file, "rb");
+
+                    if (!f) {
+                         puts(E_PROCESSINGFILE);
+                         return 1;
+                    }
+
+                    f_header_t header;
+                    file_read_header(f, &header);
+
+                    while (true) {
+                         f_data_reg_t reg = {};
+
+                         if (!file_read_data_reg(f, &reg)) {
+                              free_var_data_fields(&reg);
+
+                              long current = ftell(f);
+
+                              // Verifica se a posição atual realmente deveria ser o final do
+                              // arquivo, de acordo com o registro de cabeçalho (duas condições):
+                              // 
+                              //  - o campo `next_byte_offset` é 0 (arquivo não tem registros de dados),
+                              //    logo, a posição atual deveria ser a posição imediatamente após o cabeçalho
+                              //
+                              //  - a posição atual é igual à próxima posição livre 
+                              if ((header.next_byte_offset == 0 && current != sizeof(PACKED(f_header_t)))
+                                     || current != header.next_byte_offset) {
+                                   puts(E_PROCESSINGFILE);
+                                   return 1;
+                              }
+
+                              break;
+                         }
+
+                         /* ... */
+                         
+                         free_var_data_fields(&reg);
+                    }
+
+                    // Imprime o hash do arquivo, equivalente à função binarioNaTela
+                    printf("%lf\n", hash_file(f));
+               }
+               break;
+          case FUNC_SELECT_WHERE:
+               /* ... */
+               break;
+     }
 }
