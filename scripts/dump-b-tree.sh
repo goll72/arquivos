492244821d09f8e3eb3108aa5791580063dfcb20
goll <joaog.bastos@protonmail.ch>
2025-06-13 22:39:43 -0300

    scripts/dump-b-tree.sh: permitir que o tamanho da página seja especificado

--- scripts/dump-b-tree.sh
+++ scripts/dump-b-tree.sh
@@ -2,22 +2,66 @@
 
 set -e
 
-FILE="$1"
+while :; do
+    case "$1" in
+        -C)
+            CHAR_FMT=1
+            ;;
+        -p)
+            PAGE_SIZE=$2
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            break
+            ;;
+    esac
 
-split -a 3 -b 44 -d -- "$FILE" "$FILE".
+    shift
+done
+
+FILE="$1"
+PAGE_SIZE=${PAGE_SIZE:-44}
+
+split -a 3 -b "$PAGE_SIZE" -d -- "$FILE" "$FILE".
 
 printf '      '
 
-hexdump -L=always -e '44/1 " %02x_L[blue@0,green@1-4,yellow@5-8,cyan@9-12,gray@13-43]" " "' -e '44/1 "%_p" "\n"' "$FILE".000
+len=0
+
+if [ -n "$CHAR_FMT" ]; then
+    SEP=" "
+else
+    SEP="\n"
+fi
+
+hexdump -L=always -e "$PAGE_SIZE/1 \" %02x_L[blue@0,green@1-4,yellow@5-8,cyan@9-12,gray@13-$((PAGE_SIZE - 1))]\" \"$SEP\"" ${CHAR_FMT:+-e} ${CHAR_FMT:+"$PAGE_SIZE/1 \"%_p\" \"\n\""} "$FILE".000
 rm "$FILE".000
 
-echo
-echo "       type        n_keys      child       key         offset                  child       key         offset                  child"
+printf '\n'
+printf '       type        n_keys      child'
+
+# type + n_keys + child
+len=$((len + 12))
+
+hex_colors="gray@0-3,green@4-7,blue@8-11"
+
+while [ $((len + 16)) -le $PAGE_SIZE ]; do
+    printf '       key         offset                  child'
+
+    hex_colors="$hex_colors,yellow@$len-$((len + 3)),red@$((len + 4))-$((len + 11)),blue@$((len + 12))-$((len + 15))"
+    len=$((len + 16))
+done
+
+printf '\n'
 
 i=0
 
 for f in "$FILE".*; do
-    hexdump -L=always -e '44/1 " %02x_L[gray@0-3,green@4-7,blue@8-11,yellow@12-15,red@16-23,blue@24-27,yellow@28-31,red@32-39,blue@40-43]" " "' -e '44/1 "%_p" "\n"' "$f"
+    hexdump -L=always -e "$PAGE_SIZE/1 \" %02x_L[$hex_colors]\" \"$SEP\"" ${CHAR_FMT:+-e} ${CHAR_FMT:+"$PAGE_SIZE/1 \"%_p\" \"\n\""} "$f"
 done | while read -r line; do
     printf '%05d %s\n' $i "$line"
     i=$((i + 1))
